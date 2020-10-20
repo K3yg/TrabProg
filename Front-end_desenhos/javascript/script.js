3eb7e77d3e4e46a9504bd99595dfb3cac3f452ac
K3yg <60951410+K3yg@users.noreply.github.com>
2020-10-19 22:55:26 -0300

    Add files via upload

--- Front-end_desenhos/javascript/script.js
+++ Front-end_desenhos/javascript/script.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+  
   $("#link_listar_desenhos").click(function() {
     $.ajax({
       url: 'http://localhost:5000/listar_desenhos',
@@ -9,18 +10,47 @@ $( document ).ready(function() {
           alert("erro ao ler dados, verifique o backend");
       }
   });
-
       function listar_desenhos (desenhos) {
         $('#corpoTabelaDesenhos').empty();
         for (var i in desenhos) { 
-            lin = '<tr>' + 
+            lin = '<tr id="linha_'+desenhos[i].id+'">' +
             '<td>' + desenhos[i].nome + '</td>' + 
             '<td>' + desenhos[i].data_lancamento + '</td>' + 
-            '<td>' + desenhos[i].criadores + '</td>' +
+            '<td>' +  desenhos[i].criadores + '</td>' +
             '<td>' + desenhos[i].episodios + '</td>' + 
+            '<td> <a href=# id="excluir_' + desenhos[i].id + '" ' +
+                  'class="excluir_desenho"><img style="widht:30px; height:30px;" src="images/excluir.png" '+
+                  'alt="Excluir desenho" title="Excluir desenho"></a>' + 
+                '</td>' + 
             '</tr>';
             $('#corpoTabelaDesenhos').append(lin);
     }}
+
+    $(document).on("click", ".excluir_desenho", function() {
+      var componente_clicado = $(this).attr('id');  
+      var nome_icone = "excluir_";
+      var id_desenho = componente_clicado.substring(nome_icone.length);
+      $.ajax({
+          url: 'http://localhost:5000/excluir_desenho/'+id_desenho,
+          type: 'DELETE', 
+          dataType: 'json', 
+          success: desenhoExcluido, 
+          error: erroAoExcluir
+      });
+
+      function desenhoExcluido (retorno) {
+          if (retorno.resultado == "ok") { 
+              $("#linha_" + id_desenho).fadeOut(10, function(){
+                  alert("Sucesso!");
+              });
+          } else {
+              alert(retorno.resultado + ":" + retorno.detalhes);
+          }            
+      }
+      function erroAoExcluir (retorno) {
+          alert("erro ao excluir dados, verifique o backend: ");
+      }
+  });
 
      $("#btn_incluir_desenho").click(function(){
       nome = $("#nome").val();
